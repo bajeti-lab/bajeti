742c9dd9bff45b66c47128d09d1251e3cc7db3eb
Sarah Mukuti <cesarwrites24@gmail.com>
2023-07-18 14:02:29 +0300

    reinitialize project

---
+++ bajetiapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+'''Creates AccuntInfo and BudgetInfo models
+with the required database fields '''
+'''
+class AccountInfo(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    user_budget = models.IntegerField()
+    def __str__(self):
+       return self.username
+'''
+class ExpenseInfo(models.Model):
+  expense_name = models.CharField(max_length=20)
+  cost = models.FloatField()
+  date_added = models.DateField()
+  user_expense = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
