79e3893dd7926dba7dd8dcd4e1b75671a6a60d5e
mkitbloo <mkitbloo@gmail.com>
2023-10-16 16:31:09 +0300

    initialise gui with window and containers

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"github.com/bajeti-lab/bajeti/pkg/utils"
+	"fyne.io/fyne/v2/app"
+	"github.com/bajeti-lab/bajeti/pkg/frontend"
 )
 
 func main() {
 	// create the database
-	db, err := utils.CreateDatabase()
+	// db, err := utils.CreateDatabase()
 
-	if err != nil {
-		panic("failed to connect database")
-	}
+	// if err != nil {
+	// 	panic("failed to connect database")
+	// }
 
 	// create GUI for the user to interact with using fyne
+	a := app.New()
+	a.Settings().SetTheme(frontend.NewBajetiTheme())
+	w := a.NewWindow("Bajeti")
+
+	w.SetContent(frontend.MakeGUI())
+	w.ShowAndRun()
 
 }
